a27a95363997a2477d0ed9b89f54e3a1e892c5f8
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2017-11-03 14:31:52 +0900

    Add endpoint

--- app.rb
+++ app.rb
@@ -9,6 +9,13 @@ class App < Sinatra::Base
   end
 
   helpers do
+    def set_authentication
+      request.body.rewind
+      data = JSON.parse(request.body.read, symbolize_names: true)
+      @authentication = Authentication.find_by(access_key: data[:access_key], secret_key: data[:secret_key])
+
+      halt 403, "Can't Authenticate" if @authentication.nil?
+    end
   end
 
   get '/robot.txt' do
@@ -32,5 +39,13 @@ class App < Sinatra::Base
       # TODO: add flash message
       redirect to('/')
     end
+  end
+
+  post '/clock_in' do
+    set_authentication
+  end
+
+  post '/clock_out' do
+    set_authentication
   end
 end
